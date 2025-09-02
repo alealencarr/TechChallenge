e52817840ba689438e743aeaf4439227d9c9cc1c
ale.alencarr <alexandre-879@hotmail.com>
2025-09-02 13:09:59 -0300

    Add config mercado pago

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,17 @@
 using Api.Extensions;
 using Application.Common;
-using Application.Interfaces.Services;
-using HealthChecks.UI.Client;
 using Infrastructure.Configurations;
 using Infrastructure.DbContexts;
 using Infrastructure.Persistence;
-using Infrastructure.Services;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
- 
 
 builder.Services.AddEndpointsApiExplorer();
-
-
 builder.Services.AddScoped<HttpClient>();
 builder.Services.AddHttpContextAccessor();
-
-
+builder.Services.AddHttpClient();
 builder.Services.AddSwaggerGen(x =>
 {
     x.CustomSchemaIds(n => n.FullName);
@@ -33,8 +25,6 @@ builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
 var cnnStr = builder.Configuration.GetConnectionString(Configuration.ConnectionString);
 builder.Services.AddTransient<DataSeeder>();
 
-builder.Services.AddDbContext<AppDbContext>(x => { x.UseSqlServer(cnnStr); });
-
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseSqlServer(cnnStr, options =>
@@ -46,6 +36,9 @@ builder.Services.AddDbContext<AppDbContext>(x =>
         );
     });
 });
+
+
+
 
 var fileStorageSettings = new FileStorageSettings();
 fileStorageSettings.FileBasePath = builder.Environment.WebRootPath;
@@ -94,7 +87,7 @@ using (var scope = app.Services.CreateScope())
         try
         {
             await context.Database.MigrateAsync();
-            await seeder.Initialize();  
+            await seeder.Initialize();
             break;
         }
         catch (Exception ex)
