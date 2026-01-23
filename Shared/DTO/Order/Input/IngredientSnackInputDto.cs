6295dacc69fdbeb559897ef8f12c4445c3da819c
ale.alencarr <alexandre-879@hotmail.com>
2026-01-22 21:22:19 -0300

    correcao coverage

--- Shared/DTO/Order/Input/IngredientSnackInputDto.cs
+++ Shared/DTO/Order/Input/IngredientSnackInputDto.cs
@@ -1,4 +1,6 @@
-﻿ 
+﻿
+using System.Diagnostics.CodeAnalysis;
+
 namespace Shared.DTO.Order.Input;
 [ExcludeFromCodeCoverage]
 public record IngredientSnackInputDto(Guid Id, Guid IngredientId, Guid ItemId, bool Additional, int Quantity, decimal Price);
--- Shared/DTO/Order/Input/ItemOrderInputDto.cs
+++ Shared/DTO/Order/Input/ItemOrderInputDto.cs
@@ -1,4 +1,6 @@
-﻿ 
+﻿
+using System.Diagnostics.CodeAnalysis;
+
 namespace Shared.DTO.Order.Input;
 [ExcludeFromCodeCoverage]
 public record ItemOrderInputDto(Guid Id, Guid OrderId, Guid ProductId, int Quantity, decimal Price, ICollection<IngredientSnackInputDto>? IngredientsSnack); 
